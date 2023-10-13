52ce115548ac0442bd2a45c37d09886ab239a790
Andrii <55098895+nnocturnnn@users.noreply.github.com>
2023-10-12 23:23:12 -0400

    feat: add install script

---
+++ setup.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem Create virtual environment
+python -m venv myenv
+
+rem Activate the virtual environment
+myenv\Scripts\activate
+
+rem Install requirements
+pip install -r requirements.txt
