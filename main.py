9740c18e1dcb169f7eec530a9ddba5c727068727
Andrii <55098895+nnocturnnn@users.noreply.github.com>
2023-10-12 21:13:12 -0400

    feat: add entrypoint

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config import CONFIG
 from handlers import base, media
+import whisper
 
 
 bot = Bot(token=CONFIG.bot_token.get_secret_value())
+model = whisper.load_model("base")
 
 
 async def main():
